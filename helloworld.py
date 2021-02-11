48c6ac5dfddd6f979bec7d8c2f350d0ab6afee18
Sayali Joshi <sayali.joshi@magicmemories.com>
2021-02-11 00:02:52 +0000

    added the helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,17 @@
+import unittest
+
+
+# Create the class
+class MyClass():
+
+    def HelloWorld(self):
+        return "Hello World"
+
+# Create an object from the class
+o = MyClass()
+
+
+
+if __name__ == '__main__':
+    while True:
+        pass
